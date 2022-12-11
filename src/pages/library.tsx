6a7ac68b7471b1949a5084f747c550d296efa526
Darcmarc78 <marcscv4@gmail.com>
2022-12-11 13:34:01 -0800

    Update `alt` text in Library component
    "Add Features with Plugins" lesson was supposed to be learning to
    use Gatsby Plugins.
    Already installed `gatsby-plugin-image` and other peer dependencies
    like mentioned in the tutorial.

--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -7,7 +7,7 @@ const LibraryPage = () => {
     <div>
       <Layout pageTitle='Your Library' >
         <p>H E R E L I E S T H E L I B R A R Y </p>
-        <StaticImage src='../images/library-image.png' alt='library-image'/>
+        <StaticImage src='../images/library-image.png' alt='stock image of library'/>
       </Layout>
     </div>
   )
