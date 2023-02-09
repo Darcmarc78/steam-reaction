c15e20d699bc744a659a29492548a61270b5cc5d
Darcmarc78 <marcscv4@gmail.com>
2023-02-08 16:07:38 -0800

    Complete part 7 of tutorial

--- src/pages/discussion/{mdx.frontmatter__slug}.tsx
+++ src/pages/discussion/{mdx.frontmatter__slug}.tsx
@@ -3,11 +3,23 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import {SEO} from '../../components/seo'
 import { PageProps } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPostPage = ({data, children}: PageProps<Queries.BlogPostPageQuery>) => {
+  const heroImage = getImage(data.mdx?.frontmatter?.hero_image)!
   return (
     <Layout pageTitle={String(data.mdx?.frontmatter?.title)}>
-      <p>{data.mdx?.frontmatter?.date}</p>
+      <p>Posted: {data.mdx?.frontmatter?.date}</p>
+      <GatsbyImage
+        image={heroImage}
+        alt={String(data.mdx?.frontmatter?.hero_image_alt)} 
+      />
+      <p>
+        Photo Credit: {" "}
+        <a href={String(data.mdx?.frontmatter?.hero_image_credit_link)}>
+          {data.mdx?.frontmatter?.hero_image_credit_text}
+        </a>
+      </p>      
       {children}
       <Link to="/discussion">
         <p>Back</p>
@@ -19,11 +31,19 @@ const BlogPostPage = ({data, children}: PageProps<Queries.BlogPostPageQuery>) =>
 export default BlogPostPage
 
 export const query = graphql`
-  query BlogPostPage ($id: String) {
+  query BlogPostPage($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
