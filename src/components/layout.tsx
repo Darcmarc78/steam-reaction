5f42f85c8f1a178d8051b2e4933575c3fd05f669
Darcmarc78 <marcscv4@gmail.com>
2023-03-01 17:31:45 -0800

    Extract navbar into own component - Add `package-lock.jscon` back to git

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Navbar from "./navbar"
 type LayoutProps = {
   pageTitle: string
   children: React.ReactNode
@@ -8,36 +9,7 @@ type LayoutProps = {
 const Layout = ({ pageTitle, children }: LayoutProps) => {
   return (
     <div>
-      {/* @darcmarc78 - Should include custom Navbar here eventually */}
-      {/* TODO - Extract "nav" into separate component */}
-      <nav
-      <nav className="">
-        <div className="mx-auto max-w-2xl border-4 border-black">
-          <ul className="">
-            <li className="">
-              {/* logo redirect to home */}
-              <Link to="/" className="">
-                Home
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/about" className="">
-                About
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/library" className="">
-                Library
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/discussion" className="">
-                Discussion
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
       <main>
         <div className="flex justify-center flex-col">
           <h1 className="text-3xl font-bold underline">{pageTitle}</h1>
