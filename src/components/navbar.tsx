530285c8d5700cd1e05db80e3ebd9be569a8d380
Darcmarc78 <marcscv4@gmail.com>
2023-03-01 18:07:56 -0800

    Start work left justifying app title in nav.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,33 +3,30 @@ import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="">
-        <div className="mx-auto max-w-2xl border-4 border-black">
-          <ul className="">
-            <li className="">
-              {/* logo redirect to home */}
-              <Link to="/" className="">
-                Steam Reaction
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/about" className="">
-                About
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/library" className="">
-                Library
-              </Link>
-            </li>
-            <li className="">
-              <Link to="/discussion" className="">
-                Discussion
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <div className="">
+      <nav className="container mx-auto border-4 border-black">
+        <ul className="flex flex-row">
+          <li className="">
+            <Link to="/" className=" text-3xl">
+              Steam Reaction
+            </Link>
+          </li>
+          <li className="">
+            <Link to="/about" className="">
+              About
+            </Link>
+          </li>
+          <li className="">
+            <Link to="/library" className="">
+              Library
+            </Link>
+          </li>
+          <li className="">
+            <Link to="/discussion" className="">
+              Discussion
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   )
