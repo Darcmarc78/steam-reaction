a85d7611ab1cf2c50b8dc1d912fd29eb2756ed32
darcmarc78 <marcvictoria78@gmail.com>
2023-03-07 10:30:05 -0800

    Title left justified; - Add padding to other list items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,28 +4,30 @@ import { Link } from "gatsby"
 const Navbar = () => {
   return (
     <div className="">
-      <nav className="container mx-auto border-4 border-black">
+      <nav className="container mx-auto ">
         <ul className="flex flex-row">
-          <li className="">
-            <Link to="/" className=" text-3xl">
+          <li className="items-center">
+            <Link to="/" className=" px-8 text-3xl ">
               Steam Reaction
             </Link>
           </li>
-          <li className="">
-            <Link to="/about" className="">
-              About
-            </Link>
-          </li>
-          <li className="">
-            <Link to="/library" className="">
-              Library
-            </Link>
-          </li>
-          <li className="">
-            <Link to="/discussion" className="">
-              Discussion
-            </Link>
-          </li>
+          <div className="flex flex-row items-center">
+            <li className="px-4">
+              <Link to="/about" className="">
+                About
+              </Link>
+            </li>
+            <li className="px-4">
+              <Link to="/library" className="">
+                Library
+              </Link>
+            </li>
+            <li className="px-4">
+              <Link to="/discussion" className="">
+                Discussion
+              </Link>
+            </li>
+          </div>
         </ul>
       </nav>
     </div>
