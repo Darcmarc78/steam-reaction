05f4b17aad459f9154ad4f85d188848c878307b2
Darcmarc78 <marcscv4@gmail.com>
2023-03-29 10:57:36 -0700

    Work on using GatsbyImage tag

--- src/pages/steam/profile.tsx
+++ src/pages/steam/profile.tsx
@@ -5,22 +5,22 @@ import { SEO } from "../../components/seo"
 import { PageProps } from "gatsby"
 import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image"
 
-const ProfilePage = () => {
-  // const profileImage = getImage(data.steam.avatar)
+const ProfilePage = ({
+  data,
+  children,
+}: PageProps<Queries.ProfilePageQuery>) => {
+  const profileImage = getImage(data.imageSharp?.gatsbyImageData)!
   return (
     <Layout pageTitle="Steam Profile">
       <div className="flex flex-row items-center">
-        {/* <GatsbyImage
-        image={profileImage}
-        alt={"Placeholder Profile Picture"}
-        /> */}
-        <StaticImage
+        <GatsbyImage image={profileImage} alt={"Placeholder Profile Picture"} />
+        {/* <StaticImage
           src="../../images/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
           alt="stock image of library"
           placeholder="blurred"
           width={170}
           height={170}
-        />
+        /> */}
         {/* Profile Name: 
           Desktop Viewport: <h2/>, right of Profile Picture
           Mobile Viewport: <h2/>, Centered under profilepicture
@@ -35,7 +35,7 @@ const ProfilePage = () => {
         Width: Viewport span
         < Individual Steam Game> Component
       */}
-      
+
       <hr className="py-4" />
       <p className="flex flex-1 justify-center py-4 text-3xl">
         [STEAM LIBRARY COMPONENT]
@@ -48,10 +48,25 @@ const ProfilePage = () => {
   )
 }
 export default ProfilePage
+
+// = "20d663d7-151a-5c75-a853-00efe6137484"
+
+export const query = graphql`
+  query ProfilePage($id: String) {
+    imageSharp(id: { eq: $id }) {
+      id
+      gatsbyImageData
+    }
+  }
+`
+
 /* 
 export const query = graphql`
   query ProfilePage($id: String) {
-    
+    imageSharp(id: { eq: $id }) {
+      id
+      gatsbyImageData
+    }
   }
 ` */
 
