eddfe998466e6234c8e39e5d9ab07ebc58094792
Darcmarc78 <marcscv4@gmail.com>
2023-04-21 13:57:47 -0700

    Map out steps to get player summary.

---
+++ api/get-steam-user.tsx
@@ -0,0 +1,8 @@
+// Get User Id parameter passed from "Searchbar"
+
+// Call GetPlayerSummaries Steam API Method using .env variable and User_id parameter
+// Return results as JSON
+
+// Parse returned JSON object into avatar and personaname
+
+// Display avatar and personaname
