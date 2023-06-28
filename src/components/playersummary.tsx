c0f4a76925cebba86ea94dae4eb4bf79925825cd
Darcmarc78 <marcscv4@gmail.com>
2023-06-28 16:18:15 -0700

    Set state of playerSummary Object in `profile.tsx` Display avatarfull image.

--- src/components/playersummary.tsx
+++ src/components/playersummary.tsx
@@ -1,41 +1,27 @@
 import React from "react"
-import { getSteamUser } from "../../api/get-steam-user"
-import { Link } from "gatsby"
 
-type PlayerSummaryProps = {
-  steamId : String
-  personaName: String
-  playerSummary: Object
+type ProfileNameProps = {
+  personaName: string
+  imageURL: string
   children: React.ReactNode
 }
 
-
-export const PlayerSummary = ({steamId, playerSummary, personaName, children}:PlayerSummaryProps) => {
-  // Call get-steam-user with user supplied steamId
-  const test = getSteamUser(steamId, playerSummary)
-  console.log("Player Summary: ")
-  console.log(test)
+export const ProfileName = ({
+  personaName,
+  imageURL,
+  children,
+}: ProfileNameProps) => {
+  console.log(imageURL)
   return (
     <div>
       <div className="flex flex-row items-center">
-        <h1 className="flex flex-1 justify-center text-2xl">STEAM PROFILE FROM OBJECT{personaName}</h1>
+        <img src={imageURL} />
+        <h1 className="flex flex-1 justify-center text-2xl">
+          {personaName ? <p>{personaName}</p> : <p>No Steam Name</p>}
+        </h1>
       </div>
-
-      {/* Steam Library Component:
-            Horizontal Rule at top
-            Width: Viewport span
-            < Individual Steam Game> Component
-            */}
-      <hr className="py-4" />
-      <p className="flex flex-1 justify-center py-4 text-3xl">
-        [STEAM LIBRARY COMPONENT]
-      </p>
-      <Link to="/">
-        <p>Back to Home</p>
-      </Link>
-      {children}
     </div>
   )
 }
 
-export default PlayerSummary;
+export default ProfileName
--- src/pages/steam/profile.tsx
+++ src/pages/steam/profile.tsx
@@ -1,21 +1,52 @@
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 import * as React from "react"
 import Layout from "../../components/layout"
 import { SEO } from "../../components/seo"
 import PlayerSummary from "../../components/playersummary"
+const axios = require("axios")
 
-const profileName = "Placeholder Profile"
-const searchedID = "76561198161853165"
+const profileName = "Search Result"
+const steamId = "76561198161853165"
 
 const ProfilePage = () => {
-  const playerValue = 
+  const [playerSummary, setPlayerSummary] = React.useState({})
+  // Call get-steam-user with user supplied steamId
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:3000/forward-to-steam-api", {
+        params: {
+          steamIDParam: steamId,
+        },
+      })
+      .then((res: JSON) => {
+        // handle success
+        setPlayerSummary(res.data.response.players[0])
+      })
+      .catch((error: String) => {
+        // handle error
+        console.log("Error: " + error)
+      })
+  }, [])
   return (
     <Layout pageTitle={profileName}>
       {/* PlayerSummary Component */}
       <PlayerSummary
-        steamId={searchedID}
+        personaName={playerSummary.personaname}
+        imageURL={playerSummary.avatarfull}
         children={undefined}
       />
+      {/* Steam Library Component:
+            Horizontal Rule at top
+            Width: Viewport span
+            < Individual Steam Game> Component
+            */}
+      <hr className="py-4" />
+      <p className="flex flex-1 justify-center py-4 text-3xl">
+        [STEAM LIBRARY COMPONENT]
+      </p>
+      <Link to="/">
+        <p>Back to Home</p>
+      </Link>
     </Layout>
   )
 }
