ab81374c8241b11d077ff41dfb217c8ded66feee
darcmarc78 <marcvictoria78@gmail.com>
2023-09-21 14:33:00 -0700

    Get userSteamId from Input value

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,25 +1,28 @@
 import * as React from "react"
 
-// Handle formAction
-// Link to ProfilePage with entered steam id as param
-function handleSubmit() {}
-
 const Searchbar = () => {
   const [userSteamId, setUserSteamId] = React.useState("")
-  function handleSubmit(e: any) {
+
+  const handleSubmit = (event: any) => {
     // Prevent the browser from reloading the page
-    e.preventDefault()
+    event.preventDefault()
+    // Make call to get Player summary and Recently played games
+    // Link to ProfilePage with player summary and recently played games
   }
   return (
     <div className="flex flex-row items-center align-middle">
-      <form className="flex flex-row items-center">
+      <form onSubmit={handleSubmit} className="flex flex-row items-center">
         <input
           className="h-4/6 w-auto rounded-md border-slate-300 bg-black text-white placeholder-white"
           type="text"
           placeholder="Enter Steam Id"
-          onSubmit={handleSubmit}
+          value={userSteamId}
+          onChange={(e) => setUserSteamId(e.target.value)}
         />
-        <button className="h-4/6 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+        <button
+          type="submit"
+          className="h-4/6 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        >
           Search
         </button>
       </form>
